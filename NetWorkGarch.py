5d899207488211450bf8ac1bfff61e103c74966a
Zhiwei Lin <111717798+Zhiwei2799@users.noreply.github.com>
2023-12-05 15:46:53 -0500

    Update NetWorkGarch.py

--- NetWorkGarch.py
+++ NetWorkGarch.py
@@ -48,7 +48,7 @@ class Subnetwork(nn.Module):
         state_variable= x1 
         prev_cov = x2      
         for b in range(self.batch_size):
-            epsilon = torch.Tensor(np.random.multivariate_normal(mean=np.zeros(self.P), cov=np.identity(P)))
+            epsilon = torch.Tensor(np.random.multivariate_normal(mean=np.zeros(self.P), cov=np.identity(self.P)))
             curr_cov[b,:,:] = omega + alpha*prev_cov[b,:,:]+beta*(prev_cov[b,:,:]@epsilon)**2
             self.R[b, :] = mu + prev_cov[b,:,:]@ epsilon
         out = self.R
