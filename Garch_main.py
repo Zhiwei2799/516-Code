193e4d5d101f090560930d1bcbf9b691c0413040
Zhiwei Lin <111717798+Zhiwei2799@users.noreply.github.com>
2023-12-05 14:48:42 -0500

    Create Garch_main.py

---
+++ Garch_main.py
@@ -0,0 +1,17 @@
+input1 = torch.ones(1000,1)  # Assuming 10 samples and 5 features for input1
+input2 = torch.randn(1000,5, 5)  # Assuming 10 samples, 5 assets, and 5 features for input2
+
+# Model parameters
+P = 5  # Number of assets
+K = 3  # Number of stack layers
+lb = 0.0  # Lower bound for weights
+ub = 1.0  # Upper bound for weights
+Rf = 1.01  # Risk-free rate
+batch_size = 10  # Batch size
+alpha=3
+omega =3
+beta = 3
+mu = torch.Tensor((0.05,0.05,0.05,0.05,0.05))
+
+trainer = Optimize(P, K, lb, ub, Rf, batch_size, alpha, omega, beta, mu)
+trainer.train(input1, input2, max_epoch,batch_size,save_path)
