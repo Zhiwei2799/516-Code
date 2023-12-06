06d2704392fa1bcc61fb8f2aa501b5c60de83854
Zhiwei Lin <111717798+Zhiwei2799@users.noreply.github.com>
2023-12-06 03:01:30 -0500

    Update NetWork.py

--- NetWork.py
+++ NetWork.py
@@ -19,9 +19,9 @@ class PortfolioModel(nn.Module):
         for i in range(self.K):
 #             print('subnetwork {}'.format(i))
             inputs = self.stack_layers[i](inputs)
-            if i == 1:
-                score = (inputs.detach().numpy().mean()*np.exp(0.03/40)**-1 - 2)**2
-                return inputs.detach().numpy().mean()
+            # if i == 1:
+            #     score = (inputs.detach().numpy().mean()*np.exp(0.03/40)**-1 - 2)**2
+            #     return inputs.detach().numpy().mean()
         return inputs
 
 class Subnetwork(nn.Module):
