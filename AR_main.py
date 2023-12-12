71d0f173b197aeb1e43d502b3f01f3db3fe585ae
Zhiwei Lin <111717798+Zhiwei2799@users.noreply.github.com>
2023-12-12 00:45:48 -0500

    Update AR_main.py

--- AR_main.py
+++ AR_main.py
@@ -5,24 +5,21 @@ from utility import UtilityLoss
 from ModelAR import Optimize
 
 x_train_0 = torch.tensor([1])
-x_train_1 = torch.tensor([1, 1, 1, 1, 1])
-num_simulations = 20000
+x_train_1 = torch.tensor([1]).repeat(30)
+num_simulations = 80000
 x_train_0 = x_train_0.repeat(num_simulations, 1)
 x_train_1 = x_train_1.repeat(num_simulations, 1)
 
-P = 5 
-alpha = np.array([0.15,0.15,0.15,0.15,0.15])
+P = 30 
+alpha = np.tile(np.array([0.015]),30) 
 K = 10
-A = np.array([[0.15, 0, 0, 0, 0],
-              [0,0.15,0,0,0],
-              [0,0,0.15,0,0],
-              [0,0, 0,0.15,0],
-              [0,0, 0,0,0.15]])
-cov = np.array([[0.0238, 0.0027, 0.0027, 0.0027, 0.0027],
-                [0.0027, 0.0238, 0.0027, 0.0027, 0.0027],
-                [0.0027, 0.0027, 0.0238, 0.0027, 0.0027],
-                [0.0027, 0.0027, 0.0027, 0.0238, 0.0027],
-                [0.0027, 0.0027, 0.0027, 0.0027, 0.0238]])
+A = np.diag([-0.15] * 30)
+
+diagonal_value = 0.0238
+non_diagonal_value = 0.0027
+cov = np.full((30, 30), non_diagonal_value)
+np.fill_diagonal(cov , diagonal_value)
+
 R_neg1 = np.linalg.inv(np.eye(P) - A)@alpha
 R_neg1
 Rf = 1.03 
@@ -34,6 +31,6 @@ max_epoch = 10
 batch_size = 64
 trainer = Optimize(P, cov, K, lb, ub, Rf, batch_size, alpha, A)
 trainer.train(x_train_0,x_train_1, max_epoch,batch_size,save_path)
-# batch_size = M
-# trainer = Optimize(P, cov, K, lb, ub, Rf, batch_size, alpha, A)
-# trainer.test(x_test_0, x_test_1, save_path)
+batch_size = M
+trainer = Optimize(P, cov, K, lb, ub, Rf, batch_size, alpha, A)
+trainer.test(x_test_0, x_test_1, save_path)
